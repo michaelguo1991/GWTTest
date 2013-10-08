f7757c70a58e2694733408aa611c47479921d5fe
Michael Guo <michaelguo91@gmail.com>
2013-10-08 22:51:28 +0800

    modify file in master branch

--- widget/ListElement.java
+++ widget/ListElement.java
@@ -1,6 +1,7 @@
 public class ListElement {
 	private String name;
-	private String gender;
+	private int age;
+	private String firstName;
 
 	public ListElement(String name, String gender) {
 		this.name = name;
