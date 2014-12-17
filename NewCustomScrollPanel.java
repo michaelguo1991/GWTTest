63978516a9d5d01dddccd6c22c5b27139da26cd0
Michael Guo <michaelguo91@gmail.com>
2014-12-18 01:14:26 +0800

    test commit

--- NewCustomScrollPanel.java
+++ NewCustomScrollPanel.java
@@ -23,4 +23,8 @@ public class NewCustomScrollPanel extends CustomScrollPanel{
 			});
 		}
 	}
+
+	public void test() {
+		
+	}
 }
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 GWTTest
 =======
 This project is used to test git command, just for private purpose usage.
+
+#commit 1
